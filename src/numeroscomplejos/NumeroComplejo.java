b00d107a96d95c066ca6a80987e6ae256a6546b3
David <fdtoro@espe.edu.ec>
2023-11-23 07:35:55 -0500

    Vr1.1

--- src/numeroscomplejos/NumeroComplejo.java
+++ src/numeroscomplejos/NumeroComplejo.java
@@ -18,14 +18,13 @@ public class NumeroComplejo {
         String parReal;
         String parImg;
         String redux = numero;
-        
 
         //////////////////////////Quitar el sigo y guardar si existe////////////////////////////////////
         int sig = 0;
-        
+
         if ("+".equals(numero.substring(0, 1))) {
             redux = numero.substring(1);
-                 
+
         } else {
             if ("-".equals(numero.substring(0, 1))) {
                 redux = numero.substring(1);
@@ -33,25 +32,23 @@ public class NumeroComplejo {
             }
         }
         ///////////////////////////////////////////////////////////////////      
-        
+
         int pos;
         pos = redux.indexOf("+");
 
         if (pos != -1) {
             parReal = redux.substring(0, pos);
             parImg = redux.substring(pos, redux.length() - 1);
-            
 
         } else {
             pos = redux.indexOf("-");
             if (pos != -1) {
                 parReal = redux.substring(0, pos);
                 parImg = redux.substring(pos, redux.length() - 1);
-                
 
             } else {
-                System.out.println("Error");
-                System.exit(0) ;
+                System.out.println("Error formato incorecto");
+                System.exit(0);
                 parReal = "0";
                 parImg = "0";
             }
@@ -59,33 +56,36 @@ public class NumeroComplejo {
         //System.out.println(parReal);
         //System.out.println(parReal.length());
         //System.out.println(parImg);
-        
-        
+
         if (sig == 0) {
-            
-            try{
-            this.real = Integer.parseInt(parReal);
+
+            try {
+                this.real = Integer.parseInt(parReal);
+            } catch (Exception e) {
+                System.out.println("Error Real no valido");
+                System.exit(0);
             }
-            catch(Exception e){
-                System.out.println("Error Real no valido1");
-                System.exit(0) ;
-            }
-        } if (sig == 1) {
-            try{
-            this.real = Integer.parseInt(parReal)*(-1);
-            }
-            catch(Exception e){
-                System.out.println("Error Real no valido2");
-                System.exit(0) ;
+        }
+        if (sig == 1) {
+            try {
+                this.real = Integer.parseInt(parReal) * (-1);
+            } catch (Exception e) {
+                System.out.println("Error Real no valido");
+                System.exit(0);
             }
         }
 
-        this.img = Integer.parseInt(parImg);
+        try {
+            this.img = Integer.parseInt(parImg);;
+        } catch (Exception e) {
+            System.out.println("Error Imaginario no valido");
+            System.exit(0);
+        }
 
     }
 
     public String mostrar() {
-        if(this.img>=0){
+        if (this.img >= 0) {
             return (this.real + "+" + this.img + "i");
         }
         return (this.real + "" + this.img + "i");
@@ -95,7 +95,7 @@ public class NumeroComplejo {
         int r = this.real + num.getReal();
         int i = this.img + num.getImg();
 
-        if(i>=0){
+        if (i >= 0) {
             return (r + "+" + i + "i");
         }
         return (r + "" + i + "i");
